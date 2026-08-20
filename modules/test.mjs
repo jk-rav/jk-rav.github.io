dafef895b29c0d875535e0b29234a8940d16a2d4
jk-rav <knudsen.jan@gmail.com>
2026-08-20 17:17:37 +0200

    Update test.mjs
    new function test

--- modules/test.mjs
+++ modules/test.mjs
@@ -4,3 +4,14 @@ export function testFunc(arg1) {
   console.log(arg1)
   return {arg1};
 }
+export function getpoly(pts){
+var myPoly = document.createElementNS("http://www.w3.org/2000/svg", 'polygon');
+let myPtString=""
+for (const pt of pts){}
+
+let ptStr = pts.map((pt) => `${pt[0]} ${pt[1]}`).join(", ")
+myPoly.setAttribute("points", myPtString)
+//myPoly.setAttribute("fill", fill)
+//myPoly.setAttribute("stroke", stroke)
+return ptStr
+}
